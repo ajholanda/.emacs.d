5e47f8bcf484ce5f0b608ed6fe36b45947348d1e
Adriano J. Holanda <ajholanda@gmail.com>
2020-02-26 10:34:53 -0300

    doom config

--- init.el
+++ init.el
@@ -1,3 +1,17 @@
 (package-initialize)
 
 (load "~/.emacs.d/config.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (expand-region smartparens diminish use-package doom-themes))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
