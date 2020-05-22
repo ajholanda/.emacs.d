d55830db57af9317a53fbfb9f925db1220b25e51
Adriano J. Holanda <ajholanda@gmail.com>
2020-05-22 15:45:20 -0300

    fix python code indent offset

--- ajh-python.el
+++ ajh-python.el
@@ -1,12 +1,16 @@
 ;; source: https://github.com/howardabrams/dot-files/blob/master/emacs-python.org
+(setq python-tab-width 4)
+
 (use-package python
   :mode ("\\.py\\'" . python-mode)
-  :interpreter ("python" . python-mode)
+  :interpreter ("python3" . python-mode)
 
+  ;; execute before package initialization
   :init
   (setq-default indent-tabs-mode nil)
-
+ 
+  ;; execute after package initialization
   :config
-  (setq python-indent-offset 4)
+  (setq python-indent-offset python-tab-width)
   (add-hook 'python-mode-hook 'smartparens-mode)
-  (add-hook 'python-mode-hook 'color-identifiers-mode))
+)
