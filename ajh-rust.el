e3957a2d9c3feeca86e3f444d92f0d749b5ccab2
Adriano J. Holanda <ajholanda@gmail.com>
2020-05-28 08:55:39 -0300

    flycheck and rustfmt config

--- ajh-rust.el
+++ ajh-rust.el
@@ -13,7 +13,25 @@
   ;; execute after package initialization
   :config
   (add-hook 'rust-mode-hook
-            (lambda () (setq indent-tabs-mode nil)))
+            (lambda ()
+              (setq indent-tabs-mode nil)
+              (local-set-key (kbd "C-c <tab>") #'rust-format-buffer)
+              )
+            )
   (setq rust-format-on-save t)
   (define-key rust-mode-map (kbd "C-c C-c") 'rust-run)
+  )
+
+(use-package cargo
+  :ensure t
+  ;; execute after package initialization
+  :config
+  (add-hook 'rust-mode-hook 'cargo-minor-mode)
+  )
+
+(use-package flycheck-rust
+  :ensure t
+  ;; execute after package initialization
+  :config
+  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
   )
