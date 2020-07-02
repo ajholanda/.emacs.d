de825b94ae1c0cd244784fb99513b60af411f708
Adriano J. Holanda <ajholanda@gmail.com>
2020-07-02 12:10:51 -0300

    add more colors for tags

--- ajh-org.el
+++ ajh-org.el
@@ -8,8 +8,17 @@
 (setq org-agenda-files
       (file-expand-wildcards "~/orgmode/*.org"))
 
-;; TODO states and keywords for fast access
+;; TODO keywords
 (setq org-todo-keywords
-      '((sequence "TODO(t)" "|" "DONE(d)")
-        (sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f)")
-        (sequence "|" "CANCELED(c)")))
+      '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "ABORTED(a)")))
+
+;; TODO faces
+(setq org-todo-keyword-faces
+      (quote (("TODO" :foreground "medium blue" :weight bold)
+              ("APPT" :foreground "medium blue" :weight bold)
+              ("ABORTED" :foreground "dark red" :weight bold)
+              ("NOTE" :foreground "dark violet" :weight bold)
+              ("STARTED" :foreground "dark orange" :weight bold)
+              ("WAITING" :foreground "red" :weight bold)
+              ("DONE" :foreground "dark green" :weight bold)
+              ("DELEGATED" :foreground "red" :weight bold))))
